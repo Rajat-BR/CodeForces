4469ae5fdc3da59630b7ede182e281b400ed2f15
Rajat <rajatbr13@gmail.com>
2026-02-10 17:34:27 +0530

    Changes in Elephant

--- 800/elephant.cpp
+++ 800/elephant.cpp
@@ -4,16 +4,8 @@ using namespace std;
 int main(){
     int n , steps = 0;
     cin>>n;
-    if (n < 6){
-        cout << "1";
-        return 0;
-    } 
-
-    if(n%5==0) steps = n/5; 
-    else{
-        steps = (n/5); 
-        steps++;
-    }
+   
+    steps = (n+5-1)/5;
 
     cout << steps;
     return 0;
