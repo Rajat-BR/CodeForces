f56852002cbfc89aac007ddae9eb2dc22b46af4b
Rajat <rajatbr13@gmail.com>
2026-02-16 22:52:44 +0530

    Added Legs

---
+++ 800/Legs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t , n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        int ans = (n+3)/4;
+        cout << ans <<endl;
+    }
+
+    return 0;
+}
