27c59ca0c21955e8f200e45b83bba8cf6967addd
Rajat <rajatbr13@gmail.com>
2026-02-18 14:13:24 +0530

    Changes in Movig Chips

--- 900/Math/Moving_Chips.cpp
+++ 900/Math/Moving_Chips.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main(){
-    int n, even = 0;
+    int n, even = 0, x;
     cin >> n;
-    int arr[n];
-    for(int i = 0 ; i < n ; i++){
-        cin >> arr[i];
-        if((arr[i]&1)==0) even++;
+    int total = n;
+    while(n--){
+        cin >> x;
+        if((x&1)==0) even++;
     }
 
-    int odd = n - even;
-    int cost = 0;
-    if(even >= odd) cost = odd;
-    else cost = even;
-    cout << cost;
+    int odd = total - even;
+    cout << min(even, odd);
+
     return 0;
 }
