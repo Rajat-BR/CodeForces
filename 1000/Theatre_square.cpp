4b5fefa4fc70ffcc77e4afcb275480511429fd04
Rajat <rajatbr13@gmail.com>
2026-03-17 23:18:49 +0530

    Added 1000 rated problems

---
+++ 1000/Theatre_square.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m , n , a , num1 , num2;
+    cin >> m >> n >> a;
+
+    num1 = (m+a-1)/a;
+    num2 = (n+a-1)/a;
+
+    cout << num1 * num2;
+    
+    return 0;
+}
