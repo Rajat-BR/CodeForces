4b6055454d4e711c54de787c2517ff9ed3f865a1
Rajat <rajatbr13@gmail.com>
2026-03-23 18:59:54 +0530

    Added raising_bacteria

--- 1000/Theatre_square.cpp
+++ 1000/Theatre_square.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main(){
-    int m , n , a , num1 , num2;
+    int m , n , a ;
+    long long num1 , num2;
     cin >> m >> n >> a;
 
     num1 = (m+a-1)/a;
     num2 = (n+a-1)/a;
 
     cout << num1 * num2;
-    
+
     return 0;
 }
---
+++ 1000/raising_bacteria.cpp
@@ -0,0 +1,14 @@
+//CodeForces 579 A Raising Bacteria
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, count = 0;
+    cin >> n;
+    while(n){
+        n = n&(n-1);
+        count++;
+    }
+    cout << count;
+    return 0;
+}
